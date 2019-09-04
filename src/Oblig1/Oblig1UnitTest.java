160c2b5a5b3dc58c7fdc9c03bc905d16a261cbd4
Bao <bao91@protonmail.com>
2019-09-04 13:32:02 +0200

    maks-metode
    lagt til metode som gjør endringer og returner størst verdi

--- src/Oblig1/Oblig1UnitTest.java
+++ src/Oblig1/Oblig1UnitTest.java
@@ -1,5 +1,7 @@
 package Oblig1;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -10,7 +12,8 @@ class Oblig1UnitTest {
 
     @org.junit.jupiter.api.Test
     void maks() {
-        assertEquals(true, false, "Implementer maks og denne testen");
+        int[] a = {7,3,2,8};
+        assertEquals(8, Oblig1.maks(a));
     }
 
     @org.junit.jupiter.api.Test
